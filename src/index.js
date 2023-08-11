c5ee5e3135de6a5e9dad54a29e51514c6316ac25
LudmilaSokol <sluda999@yandex.ru>
2023-08-11 15:24:28 +0500

    feat: task solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let stringNum = String(Math.abs(n));
+  let result = "";
+  for (let i = 0; i < stringNum.length; i++) {
+    result = `${stringNum[i]}${result}`;
+  }
+  return result;
 }
